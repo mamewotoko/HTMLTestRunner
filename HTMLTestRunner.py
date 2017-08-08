f1eda90a1b6b2e79825d85c418490e083663e1ba
Takashi Masuyama <mamewotoko@gmail.com>
2017-08-09 06:50:29 +0900

    modify html into html5

--- HTMLTestRunner.py
+++ HTMLTestRunner.py
@@ -200,13 +200,13 @@ class Template_mixin(object):
     # ------------------------------------------------------------------------
     # HTML Template
 
-    HTML_TMPL = r"""<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+    HTML_TMPL = r"""<!DOCTYPE html>
+<html>
 <head>
     <title>%(title)s</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
     <meta name="generator" content="%(generator)s"/>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
     %(stylesheet)s
 </head>
 <body>
